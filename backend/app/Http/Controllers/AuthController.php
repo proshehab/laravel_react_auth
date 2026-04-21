d05b835c2348365bdccd16bafcb11989ac5f139c
proshehab <shehab.shakib@gmail.com>
2026-04-21 16:11:11 +0600

    backend api error  test

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'eamil' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
 }
