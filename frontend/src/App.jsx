2f13220978179289568be4b93880ca800dae7319
proshehab <shehab.shakib@gmail.com>
2026-04-24 15:39:00 +0600

    dashboard update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,22 @@ import Register from "./pages/Register"
 import Login from "./components/backend/Login"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
+import Dashboard from "./components/backend/Dashboard";
 
 function App() {
   
-
   return (
-    <>
-    
+    <>  
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/admin/login" element={<Login />} />
-       <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/admin/dashboard" element={<Dashboard />} />
       </Routes>
     
      <ToastContainer
       position="top-right"
      />
-      
     </>
   )
 }
